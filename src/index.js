f9bc2a366139f7fc4f5cfd71f8e1e1cf7ea857e3
Kalalshiva <gkalalshiva@gmail.com>
2024-08-03 11:12:16 +0530

    testing gh pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App";
 
-import App12 from "./App12";
-
+import App16 from "./App16";
+import App17 from "./App17";
+const sqrfunction =(n) =>{
+    return n*n;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<App flag = {false} />);
 //root.render(<App5 name="John"/>);
-root.render(<App12/>);
+root.render{
+<>
+<App16 val={5} fn={sqrfunction}/>
+<App17 val={7} fn={sqrfunction}/>
+</>
+};
